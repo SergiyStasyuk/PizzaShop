928de75b8ad3b3ee703465e9af8adfe95ae6cf42
SergiyStasyuk <sta_s@ukr.net>
2018-11-05 06:25:35 +0200

    Create product migration

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+set :database, {adapter: "sqlite3", database: "foo.sqlite3"}
+
+#set :database, "sqlite3:pizzashop.db"
+
+class Product < Sinatra::Base
+	register Sinatra::ActiveRecordExtension
+end
 
 get '/' do
 	erb :index
